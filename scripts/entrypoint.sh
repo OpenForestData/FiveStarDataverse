50975c4e89d37b5cf0e0076cd892f20d15fde9d2
Adrian Rupala <adrian.rupala@whiteaster.com>
2020-08-20 10:08:42 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 /app/scripts/wait_for.sh ofdAgregatorRedisFiveStar:6379 -t 15 -- echo "Redis (ofdAgregatorRedisFiveStar) is up!"
 
+printf "%s\n" "Collectstatic"
 python /app/manage.py collectstatic --noinput
 
+printf "%s\n" "Uruchamianie skryptu rate five star"
 python /app/rate_five_star.py
 
+printf "%s\n" "Uruchamianie aplikacji"
 python /app/manage.py runserver 0.0.0.0:8000
