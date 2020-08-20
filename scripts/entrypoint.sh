df8b4a4f833d61cdea1950835ffbabb80334a112
Adrian Rupala <adrixop95@gmail.com>
2020-08-20 10:20:14 +0200

    change ratefivestar to python script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,7 +5,8 @@ printf "%s\n" "Collectstatic"
 python /app/manage.py collectstatic --noinput
 
 printf "%s\n" "Uruchamianie skryptu rate five star"
-python /app/rate_five_star.py
+chmod +x /app/rate_five_star.py
+nohup /app/rate_five_star.py &
 
 printf "%s\n" "Uruchamianie aplikacji"
 python /app/manage.py runserver 0.0.0.0:8000
